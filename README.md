b17ffd7c937556386ddaff81245439b965d14807
marcocnc <marcocancelliere56@gmail.com>
2023-03-31 18:02:09 +0200

    coding user side

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 ## Procedimento
 1. Creare un array vuoto in cui si inseriranno i 5 numeri casuali;
 2. Funzione Math random di numeri compresi tra 1 e 10 per 5 volte;
-3. A questo punto i numeri casuali verranno inseriti nell'array vuoto
+3. A questo punto i numeri casuali verranno inseriti nell'array vuoto;
+4. Per stampare in pagina si dichiara una costante che creerà degli h2 al cui interno ci saranno i singoli numeri estratti dall'array.
+5. Timing function (timeout) per far scomparire i numeri e il bottone e far ricomparire un altro.
+6. Ora si deve richiamare il btnGo e si deve racchiudere ciò che scrive l'utente in una variabile che verrà trasformata in un array;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 //Variabili
-const btn = document.querySelector('.btn');
+const btnStart = document.querySelector('.btn');
 const arrNumbers = [];
+const numbersRow = document.querySelector('.numbersRow');
+const btnGo = document.querySelector('.btn-go');
+const userChoice = document.querySelector('#userChoice');
+let simonSays = document.querySelector('h1');
 
-btn.addEventListener('click', play);
-
-
-
+btnStart.addEventListener('click', play);
 
 
 //* Funzioni
@@ -13,33 +14,77 @@ btn.addEventListener('click', play);
 
 function play(){
 
+
+    // ciclo per creare 5 numeri random che verranno pushati in arrNumbers che è vuoto
     for (let i = 1; i <= 5; i++) {
-        
-        const randomNum = arrNumbers.push(uniqueRandomNumber(arrNumbers, 1,20));
+        const randomNum = uniqueRandomNumber(arrNumbers, 1,20);
+        arrNumbers.push(randomNum);
+        const outputNum = document.createElement('h2');
+        outputNum.innerHTML = randomNum;
+        console.log(randomNum[uniqueRandomNumber(arrNumbers, 1, 20)]);
+        numbersRow.append(outputNum);
     }
-    console.log('array', arrNumbers);
     
+    // timing function per far sparire i numeri e il btnStart
+    setTimeout(disappear, 2000);
 
+    // timing function per far apparire input e btnGo
+    setTimeout(appear, 2500);
 
+    btnGo.addEventListener('click', userTurn);
 
+    
+    
+    
+    
 }
 
+function userTurn(){
+    let userNum = userChoice.value;
+    let arrUserNum = userNum.split(',')
+    console.log(arrUserNum);
+    if(arrNumbers === arrUserNum){
+        console.log('hai vintoooo');
+    }else{
+        console.log('hai persoooo');
+    
+    }
+    return arrUserNum;
+}
+
+
+
+
+// generare numeri random
 function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// assicurarsi che il numero random sia unico
 function uniqueRandomNumber(arrayNumbers, min, max){
-    let isValid = false;
+    let isValid = false;    //flag
     let numRandom;
 
     while (!isValid) {
         numRandom = getRandomNumber(min, max);
-        
+        // se isValid non è false genera i numeri
 
         if (!arrayNumbers.includes(numRandom)) {
             isValid = true;
-        }
+        }   // se arrayNumbers non include il numero random isValid è true e il ciclo termina
     }
     
     return numRandom;
+}
+
+function disappear(){
+    btnStart.classList.add('d-none');
+    numbersRow.classList.add('d-none');
+    simonSays.innerHTML = '';
+}
+
+function appear(){
+    btnGo.classList.remove('d-none');
+    userChoice.classList.remove('d-none');
+    simonSays.innerHTML = '... and you?';
 }
